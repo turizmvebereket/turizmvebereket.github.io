c8fda3a12680d48e9c9e20838750e870af1cf5bb
turizmvebereket <proje689@gmail.com>
2024-06-23 19:38:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var po1 = document.querySelector("#po1")
+var po2 = document.querySelector("#po2")
+var po3 = document.querySelector("#po3")
+var po4 = document.querySelector("#po4")
+
+po1.addEventListener("click", function() {
+    window.location.href = "./page1.html"
+})
+po2.addEventListener("click", function() {
+    window.location.href = "./page2.html"
+})
+po3.addEventListener("click", function() {
+    window.location.href = "./page3.html"
+})
+po4.addEventListener("click", function() {
+    window.location.href = "./page4.html"
+})
